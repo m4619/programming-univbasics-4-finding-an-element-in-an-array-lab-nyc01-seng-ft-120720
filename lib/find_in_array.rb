9a21369377dd43bc415aa5a934c242fb6707cda3
Miguel  Arreola <marreola4619@gmail.com>
2020-11-09 22:35:12 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,12 +1,2 @@
 def find_element_index(array, value_to_find)
-  count = 0 
-  found_value_index = nil 
-  while count < hill.length do
-  if hill[count] == 3
-    found_value_index = count
-    end
-  end
-  count += 1
-  end
-  found_value_index
-end
+ 
