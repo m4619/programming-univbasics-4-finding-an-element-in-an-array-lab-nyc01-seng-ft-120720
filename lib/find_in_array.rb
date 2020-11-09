1649623ba6668a596b588e4f478f3ca6fc2a0b11
Miguel  Arreola <marreola4619@gmail.com>
2020-11-09 22:33:51 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,13 @@
 def find_element_index(array, value_to_find)
   hill = [1,2,3,4,5,4,3,2,1]
   count = 0 
+  found_value_index = nil 
   while count < hill.length do
   if hill[count] == 3
-    return == count
+    found_value_index = count
+    end
   end
   count += 1
   end
+  found_value_index
 end
