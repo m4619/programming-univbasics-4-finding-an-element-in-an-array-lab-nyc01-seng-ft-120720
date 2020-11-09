84c61fa2e98d0bf19ea7ae2779d1732049a6ef8d
Miguel  Arreola <marreola4619@gmail.com>
2020-11-09 22:29:10 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,12 +1,10 @@
 def find_element_index(array, value_to_find)
   scale = [2,3,4,5]
   count = 0 
-  found_value_index = nil
   while count < scale.length do
   if scale[count] == 3
-    found_value_index == count
+    return == count
     end
   count += 1
   end
-  found_value_index
 end
